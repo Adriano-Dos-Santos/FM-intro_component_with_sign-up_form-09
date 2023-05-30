f6e861fc283d186c08e4bd596bfccf631bb56afb
Adriano Dos Santos <adsc999@gmail.com>
2023-05-30 20:12:45 -0300

    userValidationComplete

--- main.js
+++ main.js
@@ -1,78 +1,76 @@
-const form = document.querySelector("form");
+const form = document.getElementById("form-container");
+const inputField = document.querySelector(".input-field");
 const inputUserFirstName = document.getElementById("first-name");
 const inputUserLastName = document.getElementById("last-name");
 const inputUserEmail = document.getElementById("email-address");
 const inputPassword = document.getElementById("password");
-const warningLabel = documnet.querySelector(".warningLabel");
+const warningLabel = document.querySelector(".warningLabel");
 let alreadyPressed = 0;
 
-/* email validation function */
+
+/* validation functions */
+
+
+function firstNameValidation() {
+    const name = inputUserFirstName.value;
+    const namePattern = /^[a-zA-Z]+$/;
+
+    if (namePattern.test(name)) {
+        return true;
+    } else {
+        return false;
+    };
+};
 
 function emailValidation() {
 
     const email = inputUserEmail.value;
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailPattern = /^[a-zA-Z0-9_%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
     if (emailPattern.test(email)) {
-        return true
+        return true;
     } else {
-        return false
+        return false;
     };
-
 };
+
 
 /*-----*/
 
-
 form.addEventListener("submit", (event) => {
- event.preventDefault()
-    if (alreadyPressed === 1) {
-        inputUserLastName.style.backgroundImage = "none";
-        inputUserLastName.style.borderColor = "rgba(10%,10%,10%,0.2)";
-        form.document.removeChild("label");
+    event.preventDefault();
+    // reset input styles everytime form is submited
+     if (alreadyPressed === 1) {
+            inputField.style.backgroundImage = "none";
+            inputField.style.borderColor = "rgba(10%,10%,10%,0.2)";
+            inputField.style.backgroundColor = "white";
+            inputField.style.color = "black";
+            warningLabel.textContent = "";
     };
-    
-  alreadyPressed = 1;
-  const userFirstName = inputUserFirstName.value;
-  const userLastName = inputUserLastName.value;
-  const userEmail = inputUserEmail.value;
-  const userPassword = inputPassword.value;
+    alreadyPressed = 1;
 
-  if (userFirstName) {
-        inputUserFirstName.style.backgroundColor = "green";
-  } else {
+    const userFirstName = inputUserFirstName.value;
+    const userLastName = inputUserLastName.value;
+    const userEmail = inputUserEmail.value;
+    const userPassword = inputPassword.value;
+    const firstNameLabel = document.getElementById("first-name-label");
+
+    // First Name Validation
+
+    if (userFirstName.length < 1) {
         inputUserFirstName.style.backgroundImage = "url(assets/icon-error.svg)";
         inputUserFirstName.style.borderColor = "hsla(0,100%,74%,1)";
-        warningLabel.innerText = "First Name cannot be empty";
-  }
-  if (userLastName) {
-    inputUserLastName.style.backgroundColor = "green";
-  } else {
-    inputUserLastName.style.backgroundImage = "url(assets/icon-error.svg)";
-    inputUserLastName.style.borderColor = "hsla(0,100%,74%,1)";
-    warningLabel.innerText = "Last Name cannot be empty";
-  };
-  if (userEmail) {
-    if (emailValidation()) {
-        inputUserEmail.style.backgroundColor = "green";
+        inputUserFirstName.style.color = "red";
+        firstNameLabel.textContent = "First Name cannot be empty";
+    } else if (!firstNameValidation()) {
+        inputUserFirstName.style.backgroundImage = "url(assets/icon-error.svg)";
+        inputUserFirstName.style.borderColor = "hsla(0,100%,74%,1)";
+        inputUserFirstName.style.color = "red";
+        firstNameLabel.textContent = "First Name is invalid";
     } else {
-     inputUserEmail.style.backgroundImage = "url(assets/icon-error.svg)";
-    inputUserEmail.style.borderColor = "hsla(0,100%,74%,1)";
-    inputUserEmail.style.color = "var(--red)"
-    warningLabel.innerText = "Looks like this is not an email";
-    
-    }
-  } else {
-    inputUserEmail.style.backgroundImage = "url(assets/icon-error.svg)";
-    inputUserEmail.style.borderColor = "hsla(0,100%,74%,1)";
-    warningLabel.innerText = "Email cannot be empty";
-  };
+        inputUserFirstName.style.backgroundColor = "green";
+    };
+    // Second Name Validation
 
-  if (userPassword) {
-    inputPassword.style.backgroundColor = "green";
-  } else {
-    inputPassword.style.backgroundImage = "url(assets/icon-error.svg)";
-    inputPassword.style.borderColor = "hsla(0,100%,74%,1)";
-    warningLabel.innerText = "Password cannot be empty";
-  };
+    
 });
