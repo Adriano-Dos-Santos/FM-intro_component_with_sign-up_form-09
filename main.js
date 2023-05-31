d49e71ece06cffd61ef4fa905848cb18fd5c0cda
Adriano Dos Santos <adsc999@gmail.com>
2023-05-31 20:28:22 -0300

    names validation completed

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const form = document.getElementById("form-container");
-const inputField = document.querySelector(".input-field");
+const inputField = document.getElementsByClassName("input-field");
+const warningLabel = document.getElementsByClassName("warningLabel");
 const inputUserFirstName = document.getElementById("first-name");
 const inputUserLastName = document.getElementById("last-name");
 const inputUserEmail = document.getElementById("email-address");
 const inputPassword = document.getElementById("password");
-const warningLabel = document.querySelector(".warningLabel");
 let alreadyPressed = 0;
 
 
 /* validation functions */
 
 
-function firstNameValidation() {
-    const name = inputUserFirstName.value;
+function nameValidation(user) {
+    const name = user.value;
     const namePattern = /^[a-zA-Z]+$/;
 
     if (namePattern.test(name)) {
@@ -21,6 +21,7 @@ function firstNameValidation() {
         return false;
     };
 };
+
 
 function emailValidation() {
 
@@ -41,36 +42,72 @@ form.addEventListener("submit", (event) => {
     event.preventDefault();
     // reset input styles everytime form is submited
      if (alreadyPressed === 1) {
-            inputField.style.backgroundImage = "none";
-            inputField.style.borderColor = "rgba(10%,10%,10%,0.2)";
-            inputField.style.backgroundColor = "white";
-            inputField.style.color = "black";
-            warningLabel.textContent = "";
+         for (let i = 0; i <= inputField.length - 1; i++) {
+
+            inputField[i].style.backgroundImage = "none";
+            inputField[i].style.borderColor = "rgba(10%,10%,10%,0.2)";
+            inputField[i].style.backgroundColor = "white";
+            inputField[i].style.color = "black";
+         }
+         for (const label of warningLabel) {
+            label.textContent = "";
+         }
     };
     alreadyPressed = 1;
 
+   // userInput = [inputUserFirstName,inputUserLastName,inputUserEmail, inputPassword];
+   // inputLabels = [firstNameLabel, lastNameLabel];
     const userFirstName = inputUserFirstName.value;
     const userLastName = inputUserLastName.value;
     const userEmail = inputUserEmail.value;
     const userPassword = inputPassword.value;
-    const firstNameLabel = document.getElementById("first-name-label");
 
+
+    const firstNameLabel = document.getElementById("first-name-label");
+    const lastNameLabel = document.getElementById("last-name-label");
+
+    const colorRed = "hsla(0,100%,74%,1)";
+
+/*
+
+    for (let i = 0; i <= 4; i++) {
+        if (userInput[i].value.length < 1) {
+        userInput[i].style.backgroundImage = "url(assets/icon-error.svg)";
+        userInput[i].style.borderColor = colorRed;
+        userInput[i].style.color = colorRed;
+        inputLabels[i].textContent = `${userInput[i]} cannot be empty`
+        };
+    };
+*/
     // First Name Validation
 
     if (userFirstName.length < 1) {
         inputUserFirstName.style.backgroundImage = "url(assets/icon-error.svg)";
-        inputUserFirstName.style.borderColor = "hsla(0,100%,74%,1)";
-        inputUserFirstName.style.color = "red";
+        inputUserFirstName.style.borderColor = colorRed;
+        inputUserFirstName.style.color = colorRed;
         firstNameLabel.textContent = "First Name cannot be empty";
-    } else if (!firstNameValidation()) {
+    } else if (!nameValidation(inputUserFirstName)) {
         inputUserFirstName.style.backgroundImage = "url(assets/icon-error.svg)";
-        inputUserFirstName.style.borderColor = "hsla(0,100%,74%,1)";
-        inputUserFirstName.style.color = "red";
+        inputUserFirstName.style.borderColor = colorRed;
+        inputUserFirstName.style.color = colorRed;
         firstNameLabel.textContent = "First Name is invalid";
     } else {
         inputUserFirstName.style.backgroundColor = "green";
     };
-    // Second Name Validation
+    // Last Name Validation
 
-    
+    if (userLastName.length < 1) {
+        inputUserLastName.style.backgroundImage = "url(assets/icon-error.svg)";
+        inputUserLastName.style.borderColor = colorRed;
+        inputUserLastName.style.color = colorRed;
+        lastNameLabel.textContent = "Last Name cannot be empty";
+    } else if (!nameValidation(inputUserLastName)) {
+        inputUserLastName.style.backgroundImage = "url(assets/icon-error.svg)";
+        inputUserLastName.style.borderColor = colorRed;
+        inputUserLastName.style.color = colorRed;
+        lastNameLabel.textContent = "Last Name is invalid";
+    } else {
+        inputUserLastName.style.backgroundColor = "green";
+    }
+
 });
